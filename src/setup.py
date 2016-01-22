9462dde68190c38f9e9d93fb998e185747057f1c
Neil Richardson <neilr+github@ieee.org>
2016-01-22 09:03:10 -0800

    Add simple setup.py file, update .gitignore for eggs

---
+++ src/setup.py
@@ -0,0 +1,25 @@
+#!/bin/env python
+#
+# @file setup.py
+# Setup and configuration required to run the program.
+
+from setuptools import setup, find_packages
+setup(
+	# Metadata
+	author = "Neil Richardson",
+	author_email = "neilr@ieee.org",
+	description = "Dummy project demonstrating Python use and API construction",
+	name = "API_demo",
+	version = "0.0.1",
+	
+	install_requires = [
+		'httpie',
+		'nose',
+	],
+	packages = find_packages(),
+	# scripts = [
+		# 'main.py',
+	# ],
+)
+
+# EOF
