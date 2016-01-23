d5856c6e7c29b95751c3f1064fc2da1b2f5462af
Neil Richardson <neilr+github@ieee.org>
2016-01-23 14:36:59 -0800

    Stub out registering some callbacks within the view module

--- src/api/v1/views/job.py
+++ src/api/v1/views/job.py
@@ -16,13 +16,24 @@ import globals
 
 globals.app_handle = flask.Flask('demo')
 
-@globals.app_handle.route('/')
+@globals.app_handle.route('/debug')
+def debug():
+	print globals.app_handle.url_map
+	return 'Debug info printed to console.'
+
+def generate_route_string(suffix):
+	return '/ask_question/v1{0}'.format(suffix)
+
+@globals.app_handle.route(generate_route_string('/'))
 def hello():
-	return 'Hello, world!'
+	return 'Hello, world! "{0}"'.format(__name__)
 
+@globals.app_handle.route(generate_route_string('/yes'))
+def yes():
+	return 'Yes'
 
-_JOB_CALLBACKS = [
-	['/', hello],
-]
+@globals.app_handle.route(generate_route_string('/no'))
+def no():
+	return 'No'
 
 # EOF
