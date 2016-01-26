2b103d3732eae5ecc2e899447ee92e7fb4b127e5
Neil Richardson <neilr+github@ieee.org>
2016-01-25 16:20:10 -0800

    WIP: Update views with /query/, stub for /time

--- src/api/v1/views/job.py
+++ src/api/v1/views/job.py
@@ -38,21 +38,18 @@ def generate_route_string(suffix):
 def api_help():
 	# Generate message that gets displayed if an invalid URL is hit.
 	allowed_methods = [
-		('lookup_uuid', {'uuid': '00000000-00000000-00000000-00000000'}),
+		('get_status', {'uuid': '00000000-00000000-00000000-00000000'}),
 		('time', {}),
 	]
 	# response = flask.url_for('lookup_uuid', **allowed_methods[0][1])
 	response_list = []
 	for each_method in allowed_methods:
 		(function_name, function_args) = each_method
-		print "Looking up {}".format(function_name)
 		response_list.append(flask.url_for('{}'.format(function_name), **function_args))
-		print response_list
 		function_pointer = globals()[function_name]
 		# If function has no docstring, getattr('__doc__') returns None...
 		docstring = getattr(function_pointer, '__doc__')
 		if docstring:
-			print "{} has docstring".format(function_pointer.__name__)
 			response_list.append(getattr(function_pointer, '__doc__').strip())
 		else:
 			response_list.append("(No additional documentation available)")
@@ -62,8 +59,13 @@ def api_help():
 # End of help() --------------------------------------------------------------
 
 
-@global_vars.app_handle.route(generate_route_string('/<uuid>'), methods=['GET'])
-def lookup_uuid(uuid=None):
+@global_vars.app_handle.route(generate_route_string('/query/<uuid>'), methods=['GET'])
+def get_status(uuid=None):
+	"""Lookup status of a query based on UUID.
+	
+	If the job is still running, queries the minion and displays the real-time
+	status.
+	"""
 	if not uuid:
 		return ('Invalid UUID', http_status_codes.BAD_REQUEST)
 	lookup_job = job.Job.from_storage(uuid)
@@ -74,18 +76,27 @@ def lookup_uuid(uuid=None):
 			'message': "UUID '{0}' not valid".format(uuid)
 		}
 		return (str(message_obj), http_status_codes.BAD_REQUEST)
-	#return 'User requested to lookup Job ID "{0}"'.format(flask.request.)
+	if 'finished' == lookup_job.get_status():
+		return (str(lookup_job.to_json()))
+	# In progress; query the minion
+	return ("Would have queried minion for job status for {}".format(lookup_job.to_json()))
+# End of get_status() --------------------------------------------------------
 
 
-@global_vars.app_handle.route(generate_route_string('/time'), methods=['POST'])
+@global_vars.app_handle.route(generate_route_string('/time'), methods=['GET'])
 def time():
 	"""Query the minion for the current time.
 	"""
+	a = job.Job()
+	a.set_status('finished')
+	a.set_result('success')
+	a.set_message('dummy entry to test the /time interface')
+	a.send_to_storage()
+	return(str(a.to_json()))
 	return "Would query the time on a remote computer."
 # End of time() --------------------------------------------------------------
 
 
-# GET  /ask_a_minion/v1/<UUID>
 # POST /ask_a_minion/v1/time   JSON: delay=TT (seconds)                 returns UUID, then current time (after delay expires)
 # POST /ask_a_minion/v1/magic8ball  JSON: delay=TT (seconds) query=??   returns UUID, then Yes/No/Maybe
 # POST /ask_a_minion/v1/
